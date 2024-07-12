07389e4bb7cf6a91c595389ab96c520fa40162c3
devil1315 <rohit131511@authenticode.in>
2024-07-12 18:56:24 +0530

    first commit

---
+++ assets/js/owl.js
@@ -0,0 +1,106 @@
+$("#owl-carousel").owlCarousel({
+  loop: false,
+  margin: 20,
+  responsiveClass: true,
+  navText: [
+    `<img src="../assets/images/icon/left.png" alt="" class="img-fluid me-3">`,
+    `<img src="../assets/images/icon/right.png" alt="" class="img-fluid">`,
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: false,
+    },
+  },
+});
+
+jQuery(document).ready(function () {
+  jQuery("#project-logo").owlCarousel({
+    loop: true,
+    items: 6,
+    autoplay: true,
+    slideTransition: "linear",
+    autoplaySpeed: 2000,
+    smartSpeed: 2000,
+    stagePadding: 20,
+    center: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
+
+  jQuery(".owl-carousel").trigger("play.owl.autoplay", [2000]);
+
+  function setSpeed() {
+    jQuery(".owl-carousel").trigger("play.owl.autoplay", [2000]);
+  }
+
+  setTimeout(setSpeed, 1000);
+});
+
+// $("#project-logo").owlCarousel({
+//   loop: false,
+//   margin: 1,
+//   responsiveClass: true,
+//   autoplay: true,
+//   smartSpeed: 500,
+//   autoplayHoverPause: true,
+
+//   responsive: {
+//     0: {
+//       items: 1,
+//       nav: true,
+//     },
+//     600: {
+//       items: 2,
+//       nav: false,
+//     },
+//     1000: {
+//       items: 6,
+//       nav: false,
+//       loop: true,
+//     },
+//   },
+// });
+
+// $("#project-logo2").owlCarousel({
+//   loop: false,
+//   margin: 1,
+//   responsiveClass: true,
+//   autoplay: true,
+//   smartSpeed: 500,
+//   autoplayHoverPause: true,
+//   rtl: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//       nav: true,
+//     },
+//     600: {
+//       items: 2,
+//       nav: false,
+//     },
+//     1000: {
+//       items: 6,
+//       nav: false,
+//       loop: true,
+//     },
+//   },
+// });
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//   var loader = $("#preloader");
+
+//   setTimeout(function () {
+//     loader.fadeOut(1000);
+//   }, 2000);
+
+//   $(window).on("beforeunload", function () {
+//     loader.fadeIn(1000);
+//   });
+// });
