0d0ff21d16654228434fc93f55f9a6fd0e98b973
devil1315 <rohit131511@authenticode.in>
2024-07-15 18:52:04 +0530

    EOD 15-07-2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-// $(document).ready(function () {
-//   var loader = $("#preloader");
+$(document).ready(function () {
+  var loader = $("#preloader");
 
-//   setTimeout(function () {
-//     loader.fadeOut(1000);
-//   }, 2000);
+  setTimeout(function () {
+    loader.fadeOut(1000);
+  }, 2000);
 
-//   $(window).on("beforeunload", function () {
-//     loader.fadeIn(1000);
-//   });
-// });
+  $(window).on("beforeunload", function () {
+    loader.fadeIn(1000);
+  });
+});
