086a8ea9ad1f722b70b5b5090bae356a8fa06a35
devil1315 <rohit131511@authenticode.in>
2024-07-16 18:31:43 +0530

    EOD 16-07-2024

--- assets/js/owl.js
+++ assets/js/owl.js
@@ -153,3 +153,28 @@ $("#blog-owl-carousel").owlCarousel({
     },
   },
 });
+$("#marketing-slide").owlCarousel({
+  loop: false,
+  margin: 20,
+  autoplay: false,
+  responsiveClass: true,
+  navText: [
+    `<img src="../assets/images/icon/left.png" alt="" class="img-fluid me-3">`,
+    `<img src="../assets/images/icon/right.png" alt="" class="img-fluid">`,
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: false,
+    },
+  },
+});
